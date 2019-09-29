15c8ce85373efaed506670f52c5129abd9b142f2
wxl <wxl>
2019-09-29 16:48:29 +0800

    first

---
+++ src/components/Counter.vue
@@ -0,0 +1,29 @@
+<template>
+	<div>
+		<button @click="increment()">+</button>
+		<button v-on:click="decrement()">-</button>
+		<p><span>child:{{num}}</span></p>
+	</div>
+</template>
+
+<script>
+	export default{
+		props:["num"],
+		data(){
+			return{
+				msg:"hello"
+			}
+		},
+		methods:{
+			increment(){
+				this.$emit("incre")
+			},
+			decrement(){
+				this.$emit("decre")
+			}
+		}
+	}
+</script>
+
+<style>
+</style>
